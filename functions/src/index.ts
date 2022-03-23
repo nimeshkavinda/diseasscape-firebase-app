36ebbfa8ee68eec2b138c3d4f4cded24ac59884f
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-03-23 18:48:12 +0530

    add firebse configs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/", (req, res) => res.status(200).send("Hello world"));
+
+exports.app = functions.https.onRequest(app);
